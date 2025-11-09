fad9a7d6a330142a0a8e9a96d3176212966f705a
sanfengliao <1164406557@qq.com>
2025-11-09 10:52:17 +0800

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   react: true,
+  rules: {
+    'unused-imports/no-unused-vars': 'error',
+    'jsonc/sort-keys': 'off',
+  },
 })
